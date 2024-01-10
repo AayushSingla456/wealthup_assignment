9e09a1c915b0d6da30554e839765c716b8a5df67
Aayush <aayushsingla27@gmail.com>
2024-01-10 19:12:37 +0530

    final commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Code from "./components/Code";
+import Front from "./components/Front";
+
+const App = () => {
+  return (
+    <div>
+      <Front />
+      <Code />
+    </div>
+  );
+};
+
+export default App;
